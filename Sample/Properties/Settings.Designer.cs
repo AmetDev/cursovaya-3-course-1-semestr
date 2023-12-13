66834fc2aa16e935b0ca0efb3d9db18e79d06a43
AmetDev <ametsejdaliev@gmail.com>
2023-12-13 10:13:13 +0300

    fff

--- Sample/Properties/Settings.Designer.cs
+++ Sample/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace Sample.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Test.mdb")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Test.mdb")]
         public string ConnStr {
             get {
                 return ((string)(this["ConnStr"]));
